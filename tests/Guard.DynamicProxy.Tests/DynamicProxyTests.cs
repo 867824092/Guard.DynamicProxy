b6099ad520f29f7a73f7733801ac57d17a7d5ed3
撒尿牛丸 <49057777+867824092@users.noreply.github.com>
2023-04-14 11:09:04 +0800

    Add interface proxy generation unit test

--- tests/Guard.DynamicProxy.Tests/DynamicProxyTests.cs
+++ tests/Guard.DynamicProxy.Tests/DynamicProxyTests.cs
@@ -1,4 +1,5 @@
-﻿using Guard.DynamicProxy.Abstracts.Interfaces;
+﻿using System;
+using Guard.DynamicProxy.Abstracts.Interfaces;
 using Guard.DynamicProxy.Core;
 using Xunit;
 
@@ -16,6 +17,18 @@ namespace Guard.DynamicProxy.Tests {
             var calculator = ProxyGenerator.CreateClassProxy<Calculate>(new LogInterceptor());
             Assert.True( calculator.Add(1, 3) == 4);
         }
+
+        [Fact]
+        public void Should_True_Create_Interface_Proxy() {
+            ICalculate calculate = ProxyGenerator.CreateInterfaceProxy<ICalculate,Calculate>(new LogInterceptor());
+            Assert.True(calculate.Add(1, 5) == 6);
+            Assert.Throws<InvalidCastException>(() => {
+                ProxyGenerator.CreateInterfaceProxy<IEmpty, Calculate>();
+            });
+            Assert.False(calculate is IEmpty);
+        }
     }
+
+    public interface IEmpty { }
 }
 
