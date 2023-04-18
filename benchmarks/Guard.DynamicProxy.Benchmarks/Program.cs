a3f79e70ca603f5fa20c5794bd1d5bad3b52c2e3
撒尿牛丸 <49057777+867824092@users.noreply.github.com>
2023-04-18 15:53:18 +0800

    add benchmark

---
+++ benchmarks/Guard.DynamicProxy.Benchmarks/Program.cs
@@ -0,0 +1,10 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Guard.DynamicProxy.Benchmarks {
+    public class Program {
+        static void Main(string[] args) {
+            var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
+        }
+    }
+}
+
